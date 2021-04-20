f9d1d4a2c90be65f17b31f394dc49f8078d7dc68
Ben Warburton <31296386+benwarburton@users.noreply.github.com>
2021-04-19 20:29:14 -0500

    Accepts new connections

--- server.py
+++ server.py
@@ -19,3 +19,23 @@ print("Waiting for a connection...")
 
 current_id = '0'
 
+def unique_client(connection):
+    return
+
+
+
+
+
+
+
+# Accepts all new incoming connections
+while True:
+    '''
+    Retrieve details of connected client, while also
+    accepting the connection
+    '''
+    connection, address = s.accept()
+    print("New connection: ", address)
+
+    # Start a new thread for each unique client
+    start_new_thread(unique_client, (connection,))
