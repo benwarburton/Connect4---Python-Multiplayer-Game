d7d96b711c8e570130f13a670a7d2f0ebcf8d595
Ben Warburton <31296386+benwarburton@users.noreply.github.com>
2021-04-26 16:33:04 -0500

    arch

--- lib/game.py
+++ lib/game.py
@@ -1,1 +1,15 @@
 import pygame
+import server
+
+class Game:
+    def init(self):
+        return
+
+class UI:
+    def init(self):
+        return
+    def connect(self):
+        return
+    def start_game(self, game):
+        return
+
