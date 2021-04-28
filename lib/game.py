f2e753637c4c20ae1c525d4c3218f7b67cbed6be
Ben Warburton <31296386+benwarburton@users.noreply.github.com>
2021-04-27 23:56:34 -0500

    build_board()

--- lib/game.py
+++ lib/game.py
@@ -1,15 +1,40 @@
 import pygame
 import time
 import numpy as np
-from lib.network import Network
+#from lib.network import Network
 
 class UI:
+    #colors of the game. blue for the board, red and yellow for the pieces, black to show empty spaces 
+    BOARD_COLOR_BLUE = (0, 0, 255)
+    YELLOW_PIECE = (255, 255, 0)
+    RED_PIECE = (255, 0, 0)
+    EMPTY_SPACE = (255, 255, 255)
+
+    #game window size
+    WINDOW = ((800, 600))
+
+    HEIGHT = 600
+    WIDTH = 800
+
+    #other constants
+    ROWS = 6
+    COLUMNS = 7
+
+    screen = pygame.display.set_mode((WINDOW))
+
     def init(self):
-        self.width = 800
-        self.height = 600
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        self.screen = pygame.display.set_caption("Connect 4")
-        time.sleep(3)
+        pygame.display.set_caption("Connect 4")
+        self.screen.fill(self.BOARD_COLOR_BLUE)
+        pygame.display.flip()
+
+    def build_board(self):
+
+        for c in range(self.COLUMNS):
+            for r in range(self.ROWS):
+                pygame.draw.circle(self.screen, self.EMPTY_SPACE,  (c*110+70, r*95+50), 35)
+        
+        pygame.display.update()
+        
         
     def start_game(self, game):
         pygame.display.flip()
@@ -22,30 +47,22 @@ class UI:
                     gameActive = False
                     
     def connect(self, address):
-        self.net = Network(address)
-        response = self.net.connect()
+        #self.net = Network(address)
+        #response = self.net.connect()
+        return self
         
 
 
 class GameUI(UI):
-    #rows and columns of the board
-    ROWS = 6
-    COLUMNS = 7
-
-    #colors of the game. blue for the board, red and yellow for the pieces, black to show empty spaces 
-    BOARD_COLOR_BLUE = (0, 0, 255)
-    YELLOW_PIECE = (255, 255, 0)
-    RED_PIECE = (255, 0, 0)
-    EMPTY_SPACE = (0, 0, 0)
 
     def init(self):
-        gameboard = np.zeros((ROWS,COLUMNS))
+        gameboard = np.zeros((self.ROWS,self.COLUMNS))
         return gameboard
     def move_piece(self):
-
+        
         return
-    def isValidPlacement(gameboard, columnPos):
-        if (gameboard[ROWS-1][columns] == 0):
+    def isValidPlacement(self, gameboard, columnPos):
+        if (gameboard[self.ROWS-1][self.COLUMNS] == 0):
             return True
         else:
             return False
--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 from lib.game import UI
+import pygame
+import sys
+import time
 
 if __name__ == '__main__':
     game = UI()
     game.init()
-
+    game.build_board()
+    time.sleep(5)
+    '''
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                #if game.WIDTH/2 <= mouse[0] <= game.WIDTH/2+140 and game.HEIGHT/2 <= mouse[1] <= game.HEIGHT/2+40:
+                break
+    '''
