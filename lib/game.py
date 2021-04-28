ba3db3cf2130f2e2b7015f3f53884570a2cb156e
Diante Woodson <47933046+woodsond@users.noreply.github.com>
2021-04-28 01:10:38 -0500

    last smol update :). finished last GameUI funct

--- lib/game.py
+++ lib/game.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import numpy as np
-#from lib.network import Network
+from lib.network import Network
 
 class UI:
     #colors of the game. blue for the board, red and yellow for the pieces, black to show empty spaces 
@@ -9,10 +9,9 @@ class UI:
     YELLOW_PIECE = (255, 255, 0)
     RED_PIECE = (255, 0, 0)
     EMPTY_SPACE = (255, 255, 255)
+    BLACK = (0,0,0)
 
     #game window size
-    WINDOW = ((800, 600))
-
     HEIGHT = 600
     WIDTH = 800
 
@@ -20,20 +19,57 @@ class UI:
     ROWS = 6
     COLUMNS = 7
 
-    screen = pygame.display.set_mode((WINDOW))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     def init(self):
-        pygame.display.set_caption("Connect 4")#set the window title to given string 
-        self.screen.fill(self.BOARD_COLOR_BLUE)#sets the color of the board to blue
+        pygame.init()
+        self.build_board()
+        self.build_main_menu()
         pygame.display.flip()
 
     def build_board(self):
+        self.screen.fill(self.BOARD_COLOR_BLUE)
+        pygame.display.set_caption("Connect 4")
 
         for c in range(self.COLUMNS):
             for r in range(self.ROWS):
                 pygame.draw.circle(self.screen, self.EMPTY_SPACE,  (c*110+70, r*95+50), 35) #draws a cirlce on the the screen in 6x7 board on the screen, and then gives it the size (length and width)
         
         pygame.display.update()
+
+    def build_main_menu(self):
+        # init font
+        font = pygame.font.SysFont('timesnewroman', 90)
+
+        # build title
+        title = font.render('Connect4', True, self.YELLOW_PIECE, self.BOARD_COLOR_BLUE)
+        title_background = title.get_rect()
+        title_background.center = (400, 150)
+        # display title
+        self.screen.blit(title, title_background)
+
+        # build menu bar
+        pygame.draw.rect(self.screen, self.BLACK, pygame.Rect(0, 330, 800, 150))
+        pygame.display.flip()
+
+        # build connect button
+        connect_text = font.render('Online', True, self.RED_PIECE)
+        connect_text_background = connect_text.get_rect()
+        connect_text_background.center = (220, 400)
+        # display connect button
+        self.screen.blit(connect_text, connect_text_background)
+
+        # build start game button
+        start_text = font.render('Start', True, self.RED_PIECE)
+        start_text_background = start_text.get_rect()
+        start_text_background.center = (580, 400)
+        # display start game button
+        self.screen.blit(start_text, start_text_background)
+
+    def start(self):
+        # listen for which main menu button is picked, then
+        # send to specified method
+        return self
         
         
     def start_game(self, game):
@@ -47,9 +83,10 @@ class UI:
                     gameActive = False
                     
     def connect(self, address):
-        #self.net = Network(address)
-        #response = self.net.connect()
-        return self
+        self.net = Network(address)
+        response = self.net.connect()
+        
+        
         
 
 
@@ -70,13 +107,13 @@ class GameUI(UI):
             return False
 
     def getNextOpenRow(self, gameboard, column):
-
-        return 
+        for row in range(self.ROWS):
+            if (gameboard[row][column] == 0):
+                    return row
 
     def win_condition(self, gameboard, pieces):
         #this will check the win condition of the player and see if they have a connect 4 horizontally 
         for column in range(self.COLUMNS-3):
-
             for row in range(self.ROWS):
 
                 if (gameboard[row][column] == pieces) and # as you can see, incrementing the j will check the respective rows for the connect 4
@@ -87,7 +124,6 @@ class GameUI(UI):
         
         #this will check the win condition of the player and see if they have a connect 4 vertically
         for column in range(self.COLUMNS):
-
             for row in range(self.ROWS-3):
 
                 if (gameboard[row][column] == pieces) and #as you can see, incrementing the i will check the respective columns for the connect 4
@@ -98,7 +134,6 @@ class GameUI(UI):
         
         #this will check the win condition of the player and see if they have a connect 4 traveling "positively" in a diagonal direction
         for column in range(self.COLUMNS-3):
-
             for row in range(self.ROWS-3):
 
                 if (gameboard[row][column] == pieces) and #as you can see, inrementing the i AND j will check the respective columns and rows heading in a diagonal, positive direction for the connect 4
@@ -110,7 +145,6 @@ class GameUI(UI):
         
         #this will check the win condition of the player and see if they have a connect 4 traveling "negatively" in a diagonal direction
         for column in range(self.COLUMNS-3):
-
             for row in range(3, self.ROWS):
 
                 if (gameboard[row][column] == pieces) and 
