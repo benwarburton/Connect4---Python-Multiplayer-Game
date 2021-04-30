49541a7c4b8e9f203ac23b8a03815d9aeca3a818
Diante Woodson <47933046+woodsond@users.noreply.github.com>
2021-04-30 01:09:02 -0500

    Revert "cool"
    This reverts commit bba159ac3b76114194148247ff04e661a3190d94.

--- lib/Network.py
+++ lib/Network.py
@@ -4,7 +4,6 @@ class Network:
 
     client_id = ''
 
-    #Initialize the client, host address, port, and identifier information 
     def __init__(self, host:str):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
@@ -12,14 +11,13 @@ class Network:
         self.address = (self.host, self.port)
         self.identifier = self.connect()
         
-    #SEt up the client to connect to the given host address, as well as get the client id and return it 
+
     def connect(self) -> str:
         self.client.connect(self.address)
         msg = self.client.recv(3).decode()
         self.client_id = msg
         return msg
 
-    #Send the date of the client to the server when asked, if unable to, print the socket error from the try/catch
     def send_data(self, data:str) -> str:
         try:
             self.client.send(str.encode(data))
@@ -29,7 +27,6 @@ class Network:
             print(str(e))
             return str(e)
     
-    #Server/Client awaits for the date to be decoded and returned, if unable to do so, print the socket error from the try/catch
     def await_data(self):
         while True:
             data = self.client.recv(3)
