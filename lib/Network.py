f6a3243233d170fd9d64a228322ee9ba60144581
Diante Woodson <47933046+woodsond@users.noreply.github.com>
2021-04-30 01:09:05 -0500

    Revert "Merge branch 'main' of https://github.com/benwarburton/Connect4---Python-Multiplayer-Game into main"
    This reverts commit 2fe4c3fa69608ddfd1335754ca56da2ccd0540a3, reversing
    changes made to bba159ac3b76114194148247ff04e661a3190d94.

--- lib/Network.py
+++ lib/Network.py
@@ -14,14 +14,14 @@ class Network:
 
     def connect(self) -> str:
         self.client.connect(self.address)
-        msg = self.client.recv(3).decode()
+        msg = self.client.recv(2048).decode()
         self.client_id = msg
         return msg
 
     def send_data(self, data:str) -> str:
         try:
             self.client.send(str.encode(data))
-            response = self.client.recv(3).decode()
+            response = self.client.recv(2048).decode()
             return response
         except socket.error as e:
             print(str(e))
@@ -29,11 +29,12 @@ class Network:
     
     def await_data(self):
         while True:
-            data = self.client.recv(3)
-            message = data.decode('utf-8')
-            if message is not None:
+            try:
+                data = self.client.recv(2048)
+                message = data.decode('utf-8')
                 return message
-            
-
+            except socket.error as e:
+                print(str(e))
+                return None
 
 
